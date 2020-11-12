9d6dec939c09175fb22ff8ef0a1e71621eba1356
ddianalim <dianalim1117@gmail.com>
2020-11-12 14:01:17 -0800

    part 1

--- lib/startup.rb
+++ lib/startup.rb
@@ -1,5 +1,15 @@
 require "employee"
 
 class Startup
+    attr_reader :name, :funding, :salaries, :employees
 
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = {}
+        
+        employees.each do |employee_name, |
+        Hash.new(Employee.new(Employee.name, @earnings))
+
+    end
 end
