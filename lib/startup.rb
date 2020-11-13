af1326e52aff568ac23884e7259480e82fe79094
ddianalim <dianalim1117@gmail.com>
2020-11-12 18:34:24 -0800

    initialize method
    well i overthought that!

--- lib/startup.rb
+++ lib/startup.rb
@@ -1,15 +1,47 @@
 require "employee"
-
+require "byebug"
 class Startup
     attr_reader :name, :funding, :salaries, :employees
 
     def initialize(name, funding, salaries)
         @name = name
         @funding = funding
-        @salaries = {}
-        
-        employees.each do |employee_name, |
-        Hash.new(Employee.new(Employee.name, @earnings))
+        @salaries = salaries
+        @employees = []
+    end
 
+    def valid_title?(title)
+        @salaries.keys.include?(title)
+    end
+
+    def >(startup)
+        self.funding > startup.funding
+    end
+
+    def hire(employee_name, title)
+        if !valid_title?(title)
+            return error
+        else
+            @employees << Employee.new(employee_name, title)
+        end
+    end
+
+    def size
+        @employees.length
+    end
+
+    def pay_employee(employee)
+        if funding > @salaries.sum
+            pay(@salaries[employee.title])
+            funding -= @salaries[employee.title]
+        else
+            return error
+        end
+    end
+
+    def payday
+        @employees.each do |employee|
+            pay_employee(employee)
+        end
     end
 end
